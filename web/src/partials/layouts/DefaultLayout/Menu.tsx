a45613c878ae140f115b74df76534602a3bc213f
hangdt-0262 <dao.thu.hang@sun-asterisk.com>
2023-10-10 16:35:42 +0700

    update sign-out

--- web/src/partials/layouts/DefaultLayout/Menu.tsx
+++ web/src/partials/layouts/DefaultLayout/Menu.tsx
@@ -3,16 +3,26 @@
 import MenuItem from "@/components/MenuItem";
 import { usePathname, useRouter } from "next/navigation";
 import DefaultMenu from "@/constants/DefaultMenu";
-import { FaHamburger } from "react-icons/fa";
+import { FaArrowRightToBracket } from "react-icons/fa6";
+import { useDispatch } from "react-redux";
+import { checkLogedInAction } from "@/store/actions/authActions";
+import { toast } from "react-toastify";
 
 export default function Menu() {
   const pathname = usePathname();
   const router = useRouter();
+  const dispatch = useDispatch();
 
   const handleClickMenuItem = (href: string) => {
     if (href) {
       router.push(href);
     }
+  };
+
+  const handleSignOut = () => {
+    localStorage.removeItem("token");
+    toast.success("Signed out!");
+    dispatch(checkLogedInAction() as any);
   };
 
   return (
@@ -33,7 +43,11 @@ export default function Menu() {
         </div>
       </nav>
       <div className="absolute 3xl:bottom-[40px] bottom-[calc(40px/6*5)]">
-        <MenuItem prefixIcon={<FaHamburger />} label="More" />
+        <MenuItem
+          prefixIcon={<FaArrowRightToBracket />}
+          onClick={handleSignOut}
+          label="Sign out"
+        />
       </div>
     </>
   );
