5cceb88416858b9783c626ac449a787ec1a705c7
tungjav <tungchuyenthainguyen@gmail.com>
2023-11-02 21:26:59 +0700

    update page.tsx

--- web/src/app/sign-in/page.tsx
+++ web/src/app/sign-in/page.tsx
@@ -16,7 +16,7 @@ import { toast } from "react-toastify";
 import { useContext } from "react";
 import { LoadingContext } from "@/providers/LoadingProvider";
 
-export const SignInForm = () => {
+export default function SignIn() {
   const {
     register,
     handleSubmit,
@@ -48,67 +48,65 @@ export const SignInForm = () => {
     }
   };
   return (
-    <div className="w-full 3xl:py-[71px] py-14">
-      <form
-        className="flex flex-col 3xl:gap-[30px] gap-[25px]"
-        onSubmit={handleSubmit(handleSubmitForm, handleErrorForm)}
-        noValidate
-      >
-        <div className="flex flex-row justify-between items-center">
-          <h1 className="font-bold 3xl:text-4xl text-3xl">Sign in</h1>
-          <span className="flex items-baseline gap-1">
-            <span>Not have account yet?</span>
-            <Link
-              href={"/sign-up"}
-              className="font-bold 3xl:text-xl text-lg text-deep-lilac"
-            >
-              Sign up
-            </Link>
-          </span>
-        </div>
-        <div className="flex flex-col 3xl:gap-[38px] gap-5">
-          <div className="flex flex-col 3xl:gap-5 gap-4">
-            <Input
-              register={register}
-              name="email"
-              type="email"
-              title="Email"
-              rules={{
-                required: true,
-              }}
-              error={errors.email?.message}
-            />
-            <Input
-              register={register}
-              name="password"
-              type="password"
-              title="Password"
-              rules={{
-                required: true,
-              }}
-              error={errors.password?.message}
-            />
-          </div>
-          <div className="flex flex-col 3xl:gap-3 gap-2">
-            <div className="ml-2">
-              <Link href="/forgot-password" className="text-azure">
-                Forgot password?
+    <AuthLayout img={femaleCatImg}>
+      <div className="w-full 3xl:py-[71px] py-14">
+        <form
+          className="flex flex-col 3xl:gap-[30px] gap-[25px]"
+          onSubmit={handleSubmit(handleSubmitForm, handleErrorForm)}
+          noValidate
+        >
+          <div className="flex flex-row justify-between items-center">
+            <h1 className="font-bold 3xl:text-4xl text-3xl">Sign in</h1>
+            <span className="flex items-baseline gap-1">
+              <span>Not have account yet?</span>
+              <Link
+                href={"/sign-up"}
+                className="font-bold 3xl:text-xl text-lg text-deep-lilac"
+              >
+                Sign up
               </Link>
-            </div>
-            <Button type="submit">Sign in</Button>
+            </span>
           </div>
+          <div className="flex flex-col 3xl:gap-[38px] gap-5">
+            <div className="flex flex-col 3xl:gap-5 gap-4">
+              <Input
+                register={register}
+                name="email"
+                type="email"
+                title="Email"
+                rules={{
+                  required: true,
+                }}
+                error={errors.email?.message}
+              />
+              <Input
+                register={register}
+                name="password"
+                type="password"
+                title="Password"
+                rules={{
+                  required: true,
+                }}
+                error={errors.password?.message}
+              />
+            </div>
+            <div className="flex flex-col 3xl:gap-3 gap-2">
+              <div className="ml-2">
+                <Link href="/forgot-password" className="text-azure">
+                  Forgot password?
+                </Link>
+              </div>
+              <Button type="submit">Sign in</Button>
+            </div>
+          </div>
+        </form>
+        <div className="3xl:mt-[48px] mt-7"></div>
+        <Divider>Or sign in with</Divider>
+        <div className="3xl:mb-[30px] mb-7"></div>
+        <div className="flex flex-col 3xl:gap-[30px] gap-4">
+          <SocialMediaButtons />
         </div>
-      </form>
-      <div className="3xl:mt-[48px] mt-7"></div>
-      <Divider>Or sign in with</Divider>
-      <div className="3xl:mb-[30px] mb-7"></div>
-      <div className="flex flex-col 3xl:gap-[30px] gap-4">
-        <SocialMediaButtons />
       </div>
-    </div>
+    </AuthLayout>
   );
-};
-
-export default function SignIn() {
-  return <AuthLayout img={femaleCatImg} children={<SignInForm />} />;
 }
