04ac3e50057563822ba7e3513229fdfe88521ef4
tungjav <tungchuyenthainguyen@gmail.com>
2023-11-04 11:21:51 +0700

    update jest.config.js

--- web/jest.config.js
+++ web/jest.config.js
@@ -5,6 +5,6 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
 };
 module.exports = createJestConfig(customJestConfig);
