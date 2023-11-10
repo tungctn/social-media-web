a35a338d01bcf6e2ea040cf4c4f0bc0ecc3343e8
tungjav <tungchuyenthainguyen@gmail.com>
2023-11-10 22:16:25 +0700

    update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -3,13 +3,13 @@ cd /home/runner/actions-runner/_work/social-media-web/social-media-web
 git pull origin main
 
 # Install dependencies nextJS
-cd web
-npm install
-npm run build
-pm2 restart nextjs-app
+# cd web
+# npm install
+# npm run build
+# pm2 restart nextjs-app
 
 # Install dependencies rails
-cd ../api
+cd api
 bundle install
 # echo "$PROD_BACKEND" > config/application.yml
 echo "tung2002" | sudo -S systemctl restart puma.service
