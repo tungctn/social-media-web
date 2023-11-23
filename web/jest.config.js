0ddaa6a9f3587858393af86051412662f5228f06
tungjav <tungchuyenthainguyen@gmail.com>
2023-11-23 16:05:59 +0700

    update web.yml, jest.config.js and package.json

--- web/jest.config.js
+++ web/jest.config.js
@@ -6,5 +6,7 @@ const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
 };
 module.exports = createJestConfig(customJestConfig);
