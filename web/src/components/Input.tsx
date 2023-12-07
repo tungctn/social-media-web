91855474254003fa552d1b83d2c400da4c4e97fe
hangdt-0262 <dao.thu.hang@sun-asterisk.com>
2023-12-07 13:48:04 +0700

    update password input

--- web/src/components/Input.tsx
+++ web/src/components/Input.tsx
@@ -1,5 +1,7 @@
 import IFormValues from "@/constants/IFormValues";
+import { useState } from "react";
 import { UseFormRegister, Validate } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa6";
 
 export type InputProps = {
   title: string;
@@ -32,19 +34,30 @@ export default function Input({
   name,
   readonly = false,
 }: InputProps) {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div>
       <label title={title} className="flex flex-col 3xl:gap-3 gap-1 w-full">
         <div className="3xl:text-xl text-lg font-bold ml-[7px]">{title}</div>
-        <input
-          type={type}
-          {...register(name, rules)}
-          placeholder={placeholder}
-          className={`3xl:rounded-[20px] rounded-[calc(20px/6*5)] 3xl:h-[57px] h-[calc(57px/6*5)] w-full bg-light-silver border-0 px-4 autofill:bg-light-silver focus:ring-deep-lilac focus:outline-none focus:border-0${
-            error && " !ring !ring-red-600/50"
-          }`}
-          readOnly={readonly}
-        />
+        <div className="relative">
+          <input
+            type={type === "password" ? (!showPassword ? type : "text") : type}
+            {...register(name, rules)}
+            placeholder={placeholder}
+            className={`3xl:rounded-[20px] rounded-[calc(20px/6*5)] 3xl:h-[57px] h-[calc(57px/6*5)] w-full bg-light-silver border-0 px-4 autofill:bg-light-silver focus:ring-deep-lilac focus:outline-none focus:border-0 ${
+              error && " !ring !ring-red-600/50"
+            }`}
+            readOnly={readonly}
+          />
+          {type === "password" && (
+            <div
+              className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </div>
+          )}
+        </div>
       </label>
       {error && <p className="text-red-600 ml-2 mt-1">{error}</p>}
     </div>
