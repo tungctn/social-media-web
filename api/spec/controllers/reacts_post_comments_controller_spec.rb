1a9563525780935682fdc28f26002e9eb58eb4dc
Ngoc Nguyen <nguyenthaingoc162002@gmail.com>
2023-12-07 12:26:33 +0700

    reacts_post_comments_controller_spec

--- api/spec/controllers/reacts_post_comments_controller_spec.rb
+++ api/spec/controllers/reacts_post_comments_controller_spec.rb
@@ -3,59 +3,60 @@ require "shared_examples"
 require "json"
 include JsonWebToken
 
-RSpec.describe ReactsPostsController, type: :controller do
+RSpec.describe ReactsPostCommentsController, type: :controller do
   describe "POST create" do
     let(:user) {create :user}
     let!(:post1) {create :post, user_id: user.id}
-    context "success create a react post" do
-      let(:reacts_post_params) {attributes_for :reacts_post, :like, user_id: user.id, post_id: post1.id}
+    let!(:post_comment) {create :post_comment, user_id: user.id, post_id: post1.id}
+    context "success create a react post comment" do
+      let(:reacts_post_comment_params) {attributes_for :reacts_post_comment, :like, user_id: user.id, comment_id: post_comment.id}
       before do
         token_new = jwt_encode({user_id: user.id, exp: Time.now.to_i + 4 * 3600})
         request.headers["Authorization"] = "Bearer #{token_new}"
-        post :create, params: reacts_post_params
+        post :create, params: reacts_post_comment_params
+      end
+
+      include_examples "should return the correct status code", 200
+
+      it "database has a new react post comment" do
+        data = JSON.parse(response.body)
+        expect(ReactsPostComment.exists?(user_id: user.id, post_comment_id: post_comment.id, react_id: 1)).to be true
+      end
+    end
+
+    context "success create a react post comment" do
+      let!(:reacts_post_comment) {create :reacts_post_comment, user_id: user.id, post_comment_id: post_comment.id}
+      before do
+        token_new = jwt_encode({user_id: user.id, exp: Time.now.to_i + 4 * 3600})
+        request.headers["Authorization"] = "Bearer #{token_new}"
+        post :create, params: {user_id: user.id, comment_id: post_comment.id, type_react: 2}
       end
 
       include_examples "should return the correct status code", 200
 
       it "database has a new post" do
         data = JSON.parse(response.body)
-        expect(ReactsPost.exists?(user_id: user.id, post_id: post1.id, react_id: 1)).to be true
+        expect(ReactsPostComment.exists?(user_id: user.id, post_comment_id: post_comment.id, react_id: 2)).to be true
       end
     end
 
-    context "success create a react post" do
-      let!(:reacts_post) {create :reacts_post, user_id: user.id, post_id: post1.id}
+    context "fail create a react post comment when react id not found" do
+      let(:reacts_post_comment_params) {attributes_for :reacts_post_comment, :react_not_found, user_id: user.id, comment_id: post_comment.id}
       before do
         token_new = jwt_encode({user_id: user.id, exp: Time.now.to_i + 4 * 3600})
         request.headers["Authorization"] = "Bearer #{token_new}"
-        post :create, params: {user_id: user.id, post_id: post1.id, type_react: 2}
-      end
-
-      include_examples "should return the correct status code", 200
-
-      it "database has a new post" do
-        data = JSON.parse(response.body)
-        expect(ReactsPost.exists?(user_id: user.id, post_id: post1.id, react_id: 2)).to be true
-      end
-    end
-
-    context "fail create a react post when react id not found" do
-      let(:reacts_post_params) {attributes_for :reacts_post, :react_not_found, user_id: user.id, post_id: post1.id}
-      before do
-        token_new = jwt_encode({user_id: user.id, exp: Time.now.to_i + 4 * 3600})
-        request.headers["Authorization"] = "Bearer #{token_new}"
-        post :create, params: reacts_post_params
+        post :create, params: reacts_post_comment_params
       end
 
       include_examples "should return the correct status code", 400
     end
 
-    context "fail create a react post when post id not found" do
-      let(:reacts_post_params) {attributes_for :reacts_post, :like, user_id: user.id, post_id: -1}
+    context "fail create a react post comment when post id not found" do
+      let(:reacts_post_comment_params) {attributes_for :reacts_post_comment, :like, user_id: user.id, comment_id: -1}
       before do
         token_new = jwt_encode({user_id: user.id, exp: Time.now.to_i + 4 * 3600})
         request.headers["Authorization"] = "Bearer #{token_new}"
-        post :create, params: reacts_post_params
+        post :create, params: reacts_post_comment_params
       end
 
       include_examples "should return the correct status code", 400
@@ -65,43 +66,44 @@ RSpec.describe ReactsPostsController, type: :controller do
   describe "DELETE destroy" do
     let(:user) {create :user}
     let!(:post1) {create :post, user_id: user.id}
-    context "success destroy a post and post like = 0" do
-      let!(:reacts_post) {create :reacts_post, user_id: user.id, post_id: post1.id}
+    let!(:post_comment) {create :post_comment, user_id: user.id, post_id: post1.id}
+    context "success destroy a react post comment and post like = 0" do
+      let!(:reacts_post_comment) {create :reacts_post_comment, user_id: user.id, post_comment_id: post_comment.id}
       before do
         token_new = jwt_encode({user_id: user.id, exp: Time.now.to_i + 4 * 3600})
         request.headers["Authorization"] = "Bearer #{token_new}"
-        delete :destroy, params: {post_id: post1.id}
+        delete :destroy, params: {comment_id: post_comment.id}
       end
 
       it "remove post from database" do
-        expect { reacts_post.reload }.to raise_error(ActiveRecord::RecordNotFound)
+        expect { reacts_post_comment.reload }.to raise_error(ActiveRecord::RecordNotFound)
       end
 
       include_examples "should return the correct status code", 200
     end
 
-    context "success destroy a post and post like > 0" do
-      let(:reacts_post_params) {attributes_for :reacts_post, :like, user_id: user.id, post_id: post1.id}
+    context "success destroy a react post comment and post like > 0" do
+      let(:reacts_post_comment_params) {attributes_for :reacts_post_comment, :like, user_id: user.id, comment_id: post_comment.id}
       before do
         token_new = jwt_encode({user_id: user.id, exp: Time.now.to_i + 4 * 3600})
         request.headers["Authorization"] = "Bearer #{token_new}"
-        post :create, params: reacts_post_params
-        delete :destroy, params: {post_id: post1.id}
+        post :create, params: reacts_post_comment_params
+        delete :destroy, params: {comment_id: post_comment.id}
       end
 
       include_examples "should return the correct status code", 200
     end
 
-    context "fail with post not found" do
-      let!(:reacts_post) {create :reacts_post, user_id: user.id, post_id: post1.id}
+    context "fail with comment not found" do
+      let!(:reacts_post_comment) {create :reacts_post_comment, user_id: user.id, post_comment_id: post_comment.id}
       before do
         token_new = jwt_encode({user_id: user.id, exp: Time.now.to_i + 4 * 3600})
         request.headers["Authorization"] = "Bearer #{token_new}"
-        delete :destroy, params: {post_id: -1}
+        delete :destroy, params: {comment_id: -1}
       end
 
       it "should not delete the football pitch" do
-        expect(ReactsPost.where(id: reacts_post.id)).to exist
+        expect(ReactsPostComment.where(id: reacts_post_comment.id)).to exist
       end
 
       include_examples "should return the correct status code", 400
@@ -111,22 +113,22 @@ RSpec.describe ReactsPostsController, type: :controller do
       before do
         token_new = jwt_encode({user_id: user.id, exp: Time.now.to_i + 4 * 3600})
         request.headers["Authorization"] = "Bearer #{token_new}"
-        delete :destroy, params: {post_id: post1.id}
+        delete :destroy, params: {comment_id: post_comment.id}
       end
 
       include_examples "should return the correct status code", 200
     end
 
     context "fail when destroy fail" do
-      let!(:reacts_post) {create :reacts_post, user_id: user.id, post_id: post1.id}
+      let!(:reacts_post_comment) {create :reacts_post_comment, user_id: user.id, post_comment_id: post_comment.id}
       before do
         token_new = jwt_encode({user_id: user.id, exp: Time.now.to_i + 4 * 3600})
         request.headers["Authorization"] = "Bearer #{token_new}"
-        allow_any_instance_of(ReactsPost).to receive(:destroy).and_return(false)
-        delete :destroy, params: {post_id: post1.id}
+        allow_any_instance_of(ReactsPostComment).to receive(:destroy).and_return(false)
+        delete :destroy, params: {comment_id: post_comment.id}
       end
       it "should not delete the football pitch" do
-        expect(ReactsPost.where(id: reacts_post.id)).to exist
+        expect(ReactsPostComment.where(id: reacts_post_comment.id)).to exist
       end
 
       include_examples "should return the correct status code", 400
